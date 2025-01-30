7c7bb4f497b95d83079dfccbc1476d807fd80f1d
shubham0730 <shubhamss0730@gmail.com>
2025-01-30 22:26:47 +0530

    homescreen-and-footer

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeScreenComponent implements OnInit {
 
+  latestJournals = [
+    { id: 1, title: 'Advancements in Oncology Research', summary: 'Exploring new methods for early cancer detection.' },
+    { id: 2, title: 'Neuroscience Breakthroughs 2024', summary: 'Recent studies on brain function and neuroplasticity.' },
+    { id: 3, title: 'COVID-19 Vaccine Developments', summary: 'An overview of vaccine research and its effectiveness.' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
